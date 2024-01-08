68faa36a4000d061bec573e21b2b980989ce6159
naoki-shigehisa <naoki-shigehisa@cookpad.com>
2024-01-08 17:21:43 +0900

    layout

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -26,13 +26,44 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: const Center(
-        child: Text(
-          'Hello, world',
-          textAlign: TextAlign.center,
-          style: TextStyle(
-            fontSize: 32,
-          ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Stack(
+              children: [
+                Container(
+                  padding: EdgeInsets.all(32),
+                  child: Image.network(
+                    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+                    height: 100,
+                    width: 100,
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.all(8),
+                  child: Text(
+                    "No.25",
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Text(
+              "Pikachu",
+              style: TextStyle(
+                fontSize: 36,
+                fontWeight: FontWeight.bold,
+              )
+            ),
+            Chip(
+              label: Text('electric'),
+              backgroundColor: Colors.yellow,
+            ),
+          ]
         ),
       ),
     );
